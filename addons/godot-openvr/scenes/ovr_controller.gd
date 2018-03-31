646cdc564f90e6f49a7edd75f5d2a14e0cdbb685
Ludvig Böklin <ludvig.boklin@protonmail.com>
2018-03-31 14:54:58 +0200

    Fix missing member var

--- addons/godot-openvr/scenes/ovr_controller.gd
+++ addons/godot-openvr/scenes/ovr_controller.gd
@@ -2,50 +2,51 @@ extends ARVRController
 
 signal controller_activated(controller)
 
+export var pad_center_radius = 0.75
 var ovr_render_model
 var components = Array()
 var ws = 0
 
 func _ready():
-    # instance our render model object
-    ovr_render_model = preload("res://addons/godot-openvr/OpenVRRenderModel.gdns").new()
+	# instance our render model object
+	ovr_render_model = preload("res://addons/godot-openvr/OpenVRRenderModel.gdns").new()
 
-    # hide to begin with
-    visible = false
+	# hide to begin with
+	visible = false
 
 func apply_world_scale():
-    var new_ws = ARVRServer.world_scale
-    if (ws != new_ws):
-        ws = new_ws
-        $Controller_mesh.scale = Vector3(ws, ws, ws)
+	var new_ws = ARVRServer.world_scale
+	if (ws != new_ws):
+		ws = new_ws
+		$Controller_mesh.scale = Vector3(ws, ws, ws)
 
 func load_controller_mesh(controller_name):
-    if ovr_render_model.load_model(controller_name.substr(0, controller_name.length()-2)):
-        return ovr_render_model
+	if ovr_render_model.load_model(controller_name.substr(0, controller_name.length()-2)):
+		return ovr_render_model
 
-    if ovr_render_model.load_model("generic_controller"):
-        return ovr_render_model
+	if ovr_render_model.load_model("generic_controller"):
+		return ovr_render_model
 
-    return Mesh.new()
+	return Mesh.new()
 
 func _process(delta):
-    if !get_is_active():
-        visible = false
-        return
+	if !get_is_active():
+		visible = false
+		return
 
-    # always set our world scale, user may end up changing this
-    apply_world_scale()
+	# always set our world scale, user may end up changing this
+	apply_world_scale()
 
-    if visible:
-        return
+	if visible:
+		return
 
-    # became active? lets handle it...
-    var controller_name = get_controller_name()
-    print("Controller " + controller_name + " became active")
+	# became active? lets handle it...
+	var controller_name = get_controller_name()
+	print("Controller " + controller_name + " became active")
 
-    # attempt to load a mesh for this
-    $Controller_mesh.mesh = load_controller_mesh(controller_name)
+	# attempt to load a mesh for this
+	$Controller_mesh.mesh = load_controller_mesh(controller_name)
 
-    # make it visible
-    visible = true
-    emit_signal("controller_activated", self)
+	# make it visible
+	visible = true
+	emit_signal("controller_activated", self)
