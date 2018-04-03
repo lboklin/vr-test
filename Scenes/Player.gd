ee19af2128e2ea3a5fbb32391a0046f35dd72b91
Ludvig Böklin <ludvig.boklin@protonmail.com>
2018-04-03 17:50:41 +0200

    Add camera mode options

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -4,14 +4,22 @@ extends ARVROrigin
 #export (NodePath) var right_hand_item = null
 
 export (NodePath) var character = null
-export var camera_distance = 2.0
-export var camera_vert_offset = 0.3
-export var camera_lock_vert = true
-export var camera_speed = 2 # meters per second
-export var camera_smooth = true
+export var camera_follow_distance = 2.0
+export var camera_follow_vert_offset = 0.3
+export var camera_follow_speed = 2 # meters per second
+export var camera_follow_smooth = true
+export var camera_mode = 0 setget set_camera_mode
+
+enum CameraMode { FREE_WALK, FREE_HOVER, FOLLOW }
+
+func set_camera_mode(mode):
+	if $HoverPlatform:
+		$HoverPlatform.visible = mode == CameraMode.FREE_HOVER
+	camera_mode = mode
 
 
 func _ready():
+	self.camera_mode = 0
 	# Grab pre-equipped items
 #	if left_hand_item:
 #		grab(get_node(left_hand_item), $Left_Hand)
@@ -21,8 +29,6 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-#	platform_movement(delta)
-
 	var head = get_node(character).find_node("Head")
 	var head_pos = head.global_transform.origin
 	var head_back = head.global_transform.basis.z.normalized()
@@ -31,32 +37,35 @@ func _process(delta):
 	var hmd_pos = hmd.global_transform.origin #/ world_scale
 	var hmd_back = hmd.global_transform.basis.z.normalized()
 
-	var v_offset = Vector3(0, head_pos.y + camera_vert_offset, 0)
-	var h_offset = (hmd_back * Vector3(1,0,1)).normalized()
-	var cam_offset = camera_distance * h_offset + v_offset #/ self.world_scale
-	var tgt_camera_pos = head_pos + cam_offset # Behind head
+	match camera_mode:
+		FREE_WALK:
+			pass
+		FREE_HOVER:
+			var platform = $HoverPlatform
+			# Rotate platform to face the player horizontal offset
+			var offset = Vector3(hmd.translation.x, 0, hmd.translation.z)
+			platform.rotation.y = Vector3(0,0,1).angle_to(offset)
+			# Move in the direction of the player's offset
+			self.global_transform.origin += offset.length() * delta * -offset * 6
+		FOLLOW:
+			var v_offset = Vector3(0, head_pos.y + camera_follow_vert_offset, 0)
+			var h_offset = (hmd_back * Vector3(1,0,1)).normalized()
+			var cam_offset = camera_follow_distance * h_offset + v_offset #/ self.world_scale
+			var tgt_camera_pos = head_pos + cam_offset # Behind head
 
-	var orig_to_hmd = hmd_pos - self.global_transform.origin
+			var orig_to_hmd = hmd_pos - self.global_transform.origin
 
-	# Move camera
-	var next_pos
-	if !camera_smooth:
-		next_pos = tgt_camera_pos
-	else:
-		var hmd_to_tgt = tgt_camera_pos - hmd_pos
-		var dir = hmd_to_tgt.normalized()
-		var dist = hmd_to_tgt.length()
-		next_pos = hmd_pos + dir * min(dist, camera_speed * delta)
-	var new_pos = next_pos - orig_to_hmd
-	self.global_transform.origin = new_pos
-
-func platform_movement(delta):
-	var hmd = $ARVRCamera
-	# Rotate platform to face the player horizontal offset
-	var offset = Vector3(hmd.translation.x, 0, hmd.translation.z)
-	$HoverPlatform.rotation.y = Vector3(0,0,1).angle_to(offset)
-	# Move in the direction of the player's offset
-	self.global_transform.origin += offset.length() * delta * -offset * 6
+			# Move camera
+			var next_pos
+			if !camera_follow_smooth:
+				next_pos = tgt_camera_pos
+			else:
+				var hmd_to_tgt = tgt_camera_pos - hmd_pos
+				var dir = hmd_to_tgt.normalized()
+				var dist = hmd_to_tgt.length()
+				next_pos = hmd_pos + dir * min(dist, camera_follow_speed * delta)
+			var new_pos = next_pos - orig_to_hmd
+			self.global_transform.origin = new_pos
 
 static func grab(item, hand):
 	var function_grab = hand.find_node("Function_Grab")
